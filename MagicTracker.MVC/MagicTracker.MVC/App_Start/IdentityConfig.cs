587b9d1f9c2b98041e5b97b25a50459ad3dbb753
Chris Crowley <cfcrowely42@gmail.com>
2021-10-16 13:12:25 -0400

    set up IdentityModels

--- MagicTracker.MVC/MagicTracker.MVC/App_Start/IdentityConfig.cs
+++ MagicTracker.MVC/MagicTracker.MVC/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using MagicTracker.MVC.Models;
+using MagicTracker.Data;
 
 namespace MagicTracker.MVC
 {
