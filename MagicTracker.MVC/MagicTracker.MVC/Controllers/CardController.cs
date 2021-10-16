6093d16b23ea91b12c2939b729d60445a25e7687
Chris Crowley <cfcrowely42@gmail.com>
2021-10-16 13:39:08 -0400

    added card index view

--- MagicTracker.MVC/MagicTracker.MVC/Controllers/CardController.cs
+++ MagicTracker.MVC/MagicTracker.MVC/Controllers/CardController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MagicTracker.Models;
 
 namespace MagicTracker.MVC.Controllers
 {
+    [Authorize]
     public class CardController : Controller
     {
         // GET: Card
         public ActionResult Index()
         {
-            return View();
+            var model = new CardListItem[0];
+            return View(model);
         }
     }
 }
--- MagicTracker.MVC/MagicTracker.Models/CardListItem.cs
+++ MagicTracker.MVC/MagicTracker.Models/CardListItem.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MagicTracker.Data;
 
 namespace MagicTracker.Models
 {
     public class CardListItem
     {
+        public int CardId { get; set; }
+        public string Name { get; set; }
+        public CardType CardType { get; set; }
+        public CardType? CardType2 { get; set; }
+        public int ManaValue { get; set; }
     }
 }
