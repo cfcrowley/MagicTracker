46a5d1abbe348b85f4d0e41514347d82fc54751d
Chris Crowley <cfcrowely42@gmail.com>
2021-10-16 20:40:51 -0400

    filled deck class

--- MagicTracker.MVC/MagicTracker.Data/Deck.cs
+++ MagicTracker.MVC/MagicTracker.Data/Deck.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MagicTracker.Data
 {
+    public enum DeckType { Standard, Legacy, Commander, Modern}
     public class Deck
     {
         [Key]
         public int DeckId { get; set; }
+        [Required]
+        public DeckType DeckType { get; set; }
+        [Required]
+        [Range(0,100)]
+        [Display(Name ="Cards in Deck")]
+        public int CardCount { get; set; }
+        [Required]
+        [Display(Name ="Theme")]
+        public string DeckStyle { get; set; }
+        public string Commander { get; set; }
+        public string Companion { get; set; }
+        [ForeignKey(nameof(Sideboard))]
+        public int SideboardId { get; set; }
+
+        public virtual List<Card> Cards { get; set; }
+        public virtual Sideboard Sideboard { get; set; }
     }
 }
